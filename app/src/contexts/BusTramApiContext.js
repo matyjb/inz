10a463cd19dbb50d178753b71234125e7fc298df
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2019-10-25 23:34:04 +0200

    .env apikeys and context function move

--- app/src/contexts/BusTramApiContext.js
+++ app/src/contexts/BusTramApiContext.js
@@ -1,5 +1,6 @@
 import React, { Component, createContext } from 'react';
-import { WARSAW_API_KEY } from 'react-native-dotenv';
+import BuildConfig from 'react-native-config';
+// import { WARSAW_API_KEY } from 'react-native-dotenv';
 
 export const BusTramApiContext = createContext();
 
@@ -9,18 +10,21 @@ export default class BusTramApiContextProvider extends Component {
     updateVehicles: this.updateVehicles
   }
   updateVehicles = () => {
-    var type = "1";
-    var resource_id = "f2e5503e927d-4ad3-9500-4ab9e55deb59";
-
-    fetch("https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id="+resource_id+"&apikey="+WARSAW_API_KEY+"&type="+type,{method: "get"})
-    .then(res => res.json())
-    .then(res => {
-      if(Array.isArray(res.result)){
-        this.setState({ vehicles: res.result })
-      }
-      else
+    if(false){ //temp for dev
+      var apikey = BuildConfig.WARSAW_API_KEY;
+      var type = "1";
+      var resource_id = "f2e5503e927d-4ad3-9500-4ab9e55deb59";
+      
+      fetch("https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id="+resource_id+"&apikey="+apikey+"&type="+type,{method: "get"})
+      .then(res => res.json())
+      .then(res => {
+        if(Array.isArray(res.result)){
+          this.setState({ vehicles: res.result })
+        }
+        else
         console.log("error: ", res.result)
-    }).catch(error => console.log("error: ", error))
+      }).catch(error => console.log("error: ", error))
+    }
   }
   componentDidMount() {
     this.interval = setInterval(this.updateVehicles, 10000);
--- app/src/screens/MapScreen.js
+++ app/src/screens/MapScreen.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
+import MapView from 'react-native-maps';
 
 export default class MapScreen extends Component {
   render() {
     return (
       <View>
         <Text> this is map screen </Text>
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     )
   }
