739b3696f6fd8c730ed6e4eb4016b6f2521f1054
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2019-12-29 18:55:52 +0100

    Stops on map + clustering

--- app/src/WarsawApi.js
+++ app/src/WarsawApi.js
@@ -1,5 +1,6 @@
 import BuildConfig from 'react-native-config';
 import React from 'react'
+import moment from 'moment';
 
 
 class WarsawApi extends React.Component {
@@ -20,7 +21,7 @@ class WarsawApi extends React.Component {
     var resource_id = "f2e5503e927d-4ad3-9500-4ab9e55deb59";
     var url = "https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id="+resource_id+"&apikey="+WarsawApi.apikey+"&type="+type+"&line="+line;
     var res = await WarsawApi._fetchApi(url,"getLine");
-
+    
     if(Array.isArray(res.result)){
       return res.result;
     }else{
@@ -32,18 +33,75 @@ class WarsawApi extends React.Component {
   static async getStops() {
     var resource_id = "ab75c33d-3a26-4342-b36a-6e5fef0a3ac3";
     var url = "https://api.um.warszawa.pl/api/action/dbstore_get?id="+resource_id+"&apikey="+WarsawApi.apikey;
-    var result = [];
-    await fetch(url,{method: "get"})
-    .then(res => res.json())
-    .then(res => {
-      if(Array.isArray(res.result)){
-        result = res.result;
+    var res = await WarsawApi._fetchApi(url,"getStops");
+    
+    if(Array.isArray(res.result)){
+      //filtering
+
+
+
+
+
+
+      console.log("result: ", res.result.length);
+      //mapping to format
+      let mapped = res.result.map(e => {
+        return {
+          unit: e.values[0].value,
+          nr: e.values[1].value,
+          name: e.values[2].value,
+          // street_id: e.values[3].value,
+          lat: Number(e.values[4].value),
+          lon: Number(e.values[5].value),
+          direction:
+            e.values[6].value == '______________________________'
+              ? ''
+              : e.values[6].value,
+          operatesSince: e.values[7].value,
+        };
+      }).filter(e => {
+        
+        if (e.name == '' ||
+          e.lat == null || isNaN(e.lat) ||
+          e.lon == null || isNaN(e.lon)) {
+            // console.log(e);
+            
+            return false;
+          }
+        return true;
+      });
+      /////
+      console.log("mapped: ", mapped.length);
+      // take newest
+      let newest = [];
+      for (let i = 0; i < mapped.length - 1; i++) {
+        const e = mapped[i];
+        const next = mapped[i + 1];
+        // same stop
+        if (e.name == next.name && e.nr == next.nr && e.unit == next.unit 
+          && moment(e.operatesSince, "YYYY-MM-DD HH:mm:ss.SSS").diff(moment(next.operatesSince, "YYYY-MM-DD HH:mm:ss.SSS")) < 0
+          ) {
+              continue;
+            }else{ 
+              newest.push(e);
+            }
       }
-      else{
-        console.log("[error] getStops: ", res.result)
-      }
-    }).catch(error => console.log("error2: ", error))
-    return result;
+      console.log("newest: ", newest.length);
+      return newest;
+
+
+
+
+
+
+
+
+      ///////////////
+    }else{
+      console.log("[error] getLine: ", res)
+      return [];
+    }
+    
   }
 
   static async getStopUnit(name) {
--- app/src/screens/MapScreen.js
+++ app/src/screens/MapScreen.js
@@ -1,19 +1,109 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import MapView, {Circle} from 'react-native-maps';
+import MapView, {Circle, Marker} from 'react-native-maps';
 import {BusTramApiContext} from '../contexts/BusTramApiContext';
 import Vehicle from '../components/Vehicle';
 import {Fab, Icon, Button, Text} from 'native-base';
 import Modal from 'react-native-modalbox';
 
 import {ThemeContext} from '../contexts/ThemeContext';
-import {ThemeConstants} from '../constants/ThemeConstants';
+import WarsawApi from '../WarsawApi';
 
 class MapScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+  state = {
+    stops: [],
+    mapBoundaries: null,
+  };
 
+  async componentDidMount() {
+    let stops = await WarsawApi.getStops();
+    console.log('got all stops');
+
+    this.setState({stops: stops});
+  }
+
+  async onRegionChangeComplete() {
+    this.setState({
+      mapBoundaries: await this.map.getMapBoundaries(),
+    });
+  }
+
+  getStopsMarkers() {
+    if (this.state.mapBoundaries) {
+      // bounding box
+      let late = this.state.mapBoundaries.northEast.latitude;
+      let lats = this.state.mapBoundaries.southWest.latitude;
+      let lone = this.state.mapBoundaries.northEast.longitude;
+      let lons = this.state.mapBoundaries.southWest.longitude;
+      let latdiff = late - lats;
+      let londiff = lone - lons;
+      late += latdiff / 3;
+      lats -= latdiff / 3;
+      lone += londiff / 3;
+      lons -= londiff / 3;
+      //
+      
+      let stopsMarkersVisible = this.state.stops.filter(e => {
+        if (lons < e.lon && e.lon < lone && lats < e.lat && e.lat < late)
+          return true;
+        return false;
+      });
+      if (stopsMarkersVisible.length == 0) return [];
+
+      let output = [];
+      console.log(latdiff);
+      if (latdiff < 0.013) {
+        // no clustering
+        output = stopsMarkersVisible;
+      } else if (latdiff < 0.04) {
+        // clustering
+        let grouped = stopsMarkersVisible.reduce((rv, x) => {
+          let v = x.unit;
+          let el = rv.find(r => r && r.unit === v);
+          if (el) {
+            el.values.push(x);
+          } else {
+            rv.push({unit: v, values: [x]});
+          }
+          return rv;
+        }, []);
+
+        output = grouped.map(unit => {
+          let l = unit.values.reduce(
+            (a, x) => {
+              return {sumlat: a.sumlat + x.lat, sumlon: a.sumlon + x.lon};
+            },
+            {sumlat: 0, sumlon: 0},
+          );
+          return {
+            ...unit.values[0],
+            lat: l.sumlat / unit.values.length,
+            lon: l.sumlon / unit.values.length,
+          };
+        });
+      } else {
+        return [];
+      }
+
+      console.log('markery', output.length);
+      return output.map(stop => {
+        let c = {latitude: stop.lat, longitude: stop.lon};
+        return (
+          <Marker
+            key={stop.unit + ':' + stop.nr}
+            coordinate={c}
+            title={stop.name + ' ' + stop.nr}
+            description={stop.unit}
+          />
+        );
+      });
+    } else {
+      return [];
+    }
+  }
   render() {
     const initRegion = {
       latitude: 52.122801,
@@ -21,93 +111,101 @@ class MapScreen extends React.Component {
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     };
+
     return (
       <ThemeContext.Consumer>
-        {({t}) => 
-            <BusTramApiContext.Consumer>
-              {({vehicles, setMapRegion, radar, toggleRadar}) => {
-                return (
-                  <>
-                    <View style={styles.container}>
-                      <MapView
-                        customMapStyle={t.mapStyle}
-                        initialRegion={initRegion}
-                        showsCompass={true}
-                        rotateEnabled={false}
-                        style={{...StyleSheet.absoluteFillObject}}
-                        onRegionChangeComplete={setMapRegion}>
-                        {vehicles.map(v => (
-                          <Vehicle
-                            key={v.Lines + '-' + v.Brigade}
-                            coordinates={{latitude: v.Lat, longitude: v.Lon}}
-                            line={v.Lines}
-                            brigade={v.Brigade}
-                          />
-                        ))}
-                        {radar.isOn && (
-                          <Circle
-                            style={{zIndex: 999}}
-                            center={radar.coordinates}
-                            radius={radar.radiusKMs * 1000}
-                            strokeWidth={1}
-                            strokeColor={t.radarStrokeColor}
-                            fillColor={t.radarFillColor}
-                          />
-                        )}
-                      </MapView>
-                    </View>
-                    <Fab
-                      active={true}
-                      containerStyle={{}}
-                      style={{backgroundColor: t.accentColor}}
-                      position="bottomRight"
-                      onPress={() => toggleRadar()}>
+        {({t}) => (
+          <BusTramApiContext.Consumer>
+            {({vehicles, setMapRegion, radar, toggleRadar}) => {
+              return (
+                <>
+                  <View style={styles.container}>
+                    <MapView
+                      ref={ref => {
+                        this.map = ref;
+                      }}
+                      customMapStyle={t.mapStyle}
+                      initialRegion={initRegion}
+                      showsCompass={true}
+                      rotateEnabled={false}
+                      style={{...StyleSheet.absoluteFillObject}}
+                      onRegionChangeComplete={nr => {
+                        setMapRegion(nr);
+                        this.onRegionChangeComplete();
+                      }}>
+                      {vehicles.map(v => (
+                        <Vehicle
+                          key={v.Lines + '-' + v.Brigade}
+                          coordinates={{latitude: v.Lat, longitude: v.Lon}}
+                          line={v.Lines}
+                          brigade={v.Brigade}
+                        />
+                      ))}
+                      {this.getStopsMarkers()}
+                      {radar.isOn && (
+                        <Circle
+                          style={{zIndex: 999}}
+                          center={radar.coordinates}
+                          radius={radar.radiusKMs * 1000}
+                          strokeWidth={1}
+                          strokeColor={t.radarStrokeColor}
+                          fillColor={t.radarFillColor}
+                        />
+                      )}
+                    </MapView>
+                  </View>
+                  <Fab
+                    active={true}
+                    containerStyle={{}}
+                    style={{backgroundColor: t.accentColor}}
+                    position="bottomRight"
+                    onPress={() => toggleRadar()}>
+                    <Icon
+                      name="radar"
+                      type="MaterialCommunityIcons"
+                      style={{color: t.primaryColor}}
+                    />
+                    <Button
+                      style={{
+                        backgroundColor: t.accentColor,
+                        marginBottom: 14,
+                      }}>
                       <Icon
-                        name="radar"
-                        type="MaterialCommunityIcons"
+                        name="gps-fixed"
+                        type="MaterialIcons"
                         style={{color: t.primaryColor}}
                       />
-                      <Button
-                        style={{
-                          backgroundColor: t.accentColor,
-                          marginBottom: 14,
-                        }}>
-                        <Icon
-                          name="gps-fixed"
-                          type="MaterialIcons"
-                          style={{color: t.primaryColor}}
-                        />
-                      </Button>
-                      <Button
-                        style={{backgroundColor: t.accentColor}}
-                        onPress={() =>
-                          this.props.navigation.navigate('Settings')
-                        }>
-                        <Icon
-                          name="md-settings"
-                          style={{color: t.primaryColor}}
-                        />
-                      </Button>
-                      <Button
-                        style={{backgroundColor: t.accentColor}}
-                        onPress={() => this._modal.open()}>
-                        <Icon name="md-menu" style={{color: t.primaryColor}} />
-                      </Button>
-                    </Fab>
-                    <Modal
-                      style={{height: 300, zIndex: 1}}
-                      position={'bottom'}
-                      backButtonClose
-                      ref={ref => {
-                        this._modal = ref;
-                      }}>
-                      <Text>Modal on bottom with backdrop</Text>
-                    </Modal>
-                  </>
-                );
-              }}
-            </BusTramApiContext.Consumer>
-        }
+                    </Button>
+                    <Button
+                      style={{backgroundColor: t.accentColor}}
+                      onPress={() =>
+                        this.props.navigation.navigate('Settings')
+                      }>
+                      <Icon
+                        name="md-settings"
+                        style={{color: t.primaryColor}}
+                      />
+                    </Button>
+                    <Button
+                      style={{backgroundColor: t.accentColor}}
+                      onPress={() => this._modal.open()}>
+                      <Icon name="md-menu" style={{color: t.primaryColor}} />
+                    </Button>
+                  </Fab>
+                  <Modal
+                    style={{height: 300, zIndex: 1}}
+                    position={'bottom'}
+                    backButtonClose
+                    ref={ref => {
+                      this._modal = ref;
+                    }}>
+                    <Text>Modal on bottom with backdrop</Text>
+                  </Modal>
+                </>
+              );
+            }}
+          </BusTramApiContext.Consumer>
+        )}
       </ThemeContext.Consumer>
     );
   }
